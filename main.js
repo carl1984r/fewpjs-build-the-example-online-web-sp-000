b542774314a91383116de854cdc3d3f5d210024f
Bry Cox <carl.cc@gmail.com>
2020-01-01 15:57:52 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
- document.querySelector('div#modal').setAttribute('class', "hidden");
+  //document.querySelector('div#modal').setAttribute('class', "hidden");
 
  function toggleHeart(heart) {
    if (heart.querySelector('.like-glyph')) { heart = heart.querySelector('.like-glyph') }
